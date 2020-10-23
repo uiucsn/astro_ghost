d5cb1cc56252eb9bb1f0b4e133fd8f862f9bd865
Alex Gagliano <alexandertgagliano@gmail.com>
2020-10-23 14:58:35 -0500

    Added helper functions into the README and switched format to read in arrays of events
    Former-commit-id: c77f9a6ea610d7adf9400f3067a7b02945e4ddaf

--- astro_ghost/tutorial.py
+++ astro_ghost/tutorial.py
@@ -1,10 +1,10 @@
-#tutorial file for Rahul for running the GHOST association algorithm
 import os
 import sys
 from astro_ghost.PS1QueryFunctions import getAllPostageStamps
 from astro_ghost.TNSQueryFunctions import getTNSSpectra
 from astro_ghost.NEDQueryFunctions import getNEDSpectra
-from astro_ghost.ghostHelperFunctions import getTransientHosts, getGHOST
+#from astro_ghost.ghostHelperFunctions import getTransientHosts, getGHOST
+from astro_ghost.ghostHelperFunctions import *
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 import pandas as pd
@@ -17,17 +17,19 @@ verbose = 1
 #note: real=False creates an empty database, which
 #allows you to use the association methods without
 #needing to download the full database first
-getGHOST(real=False, verbose=verbose)
+getGHOST(real=True, verbose=verbose)
 
 #create a list of the supernova names, their skycoords, and their classes (these three are from TNS)
 snName = ['SN 2012dt', 'SN 1998bn', 'SN 1957B']
-snCoord = [SkyCoord(14.162*u.deg, -9.90253*u.deg, frame='icrs'), SkyCoord(187.32867*u.deg, -23.16367*u.deg, frame='icrs'), SkyCoord(186.26125*u.deg, +12.899444*u.deg, frame='icrs')]
-snClass = ['SN IIP', 'SN', 'SN Ia']
+
+snCoord = [SkyCoord(14.162*u.deg, -9.90253*u.deg, frame='icrs'), \
+           SkyCoord(187.32867*u.deg, -23.16367*u.deg, frame='icrs'), \
+           SkyCoord(186.26125*u.deg, +12.899444*u.deg, frame='icrs')]
 
 # run the association algorithm!
 # this first checks the GHOST database for a SN by name, then by coordinates, and
 # if we have no match then it manually associates them.
-hosts = getTransientHosts(snName, snCoord, snClass, verbose=verbose, starcut='normal')
+hosts = getTransientHosts(snName, snCoord, verbose=verbose, starcut='normal')
 
 #create directories to store the host spectra, the transient spectra, and the postage stamps
 hSpecPath = "./hostSpectra/"
@@ -37,7 +39,6 @@ paths = [hSpecPath, tSpecPath, psPath]
 for tempPath in paths:
     if not os.path.exists(tempPath):
         os.makedirs(tempPath)
-
 
 now = datetime.now()
 dateStr = "%i%.02i%.02i" % (now.year,now.month,now.day)
@@ -49,3 +50,37 @@ transients = pd.read_csv("./transients_%s/tables/%s"%(dateStr,fn_SN))
 getAllPostageStamps(hosts, 120, psPath, verbose) #get postage stamps of hosts
 getNEDSpectra(hosts, hSpecPath, verbose) #get spectra of hosts
 getTNSSpectra(transients, tSpecPath, verbose) #get spectra of transients (if on TNS)
+
+# Helper functions for querying the database
+supernovaCoord = [SkyCoord(344.5011708333333*u.deg, 6.0634388888888875*u.deg, frame='icrs')]
+galaxyCoord = [SkyCoord(344.50184181*u.deg, 6.06983149*u.deg, frame='icrs')]
+snName = ["PTF10llv"]
+table = fullData()
+
+# 1. Get the entry corresponding to a specific transient by its name (or coordinates)
+#    note: The coordinate/name is passed as a list, so multiple entries can be
+#          queried simultaneously
+#    This function returns the matches as a pandas dataframe (df) along with
+#    a list of the sources not found (by name or coordinate)
+df, notFound = getDBHostFromTransientCoords(supernovaCoord)
+df, notFound = getDBHostFromTransientName(snName)
+
+# 2. Print summary statistics about a particular host galaxy system or set of systems from a supernova
+getHostStatsFromTransientName(snName)
+getHostStatsFromTransientCoords(supernovaCoord)
+
+# 3. Get stats about the supernovae associated with a host galaxy
+galaxyName = ['UGC 12266']
+getTransientStatsFromHostName(galaxyName)
+getTransientStatsFromHostCoords(galaxyCoord)
+
+# 4. get an image of the field by coordinates
+tempSize = 400 #size in pixels
+band = ['grizy']
+getcolorim(galaxyCoord[0].ra.deg, galaxyCoord[0].dec.deg, size=tempSize, filters=band, format="png")
+
+# 5. get an image of the host galaxy system associated with a supernova (by supernova name)
+getHostImage(snName, save=0)
+
+# 6. Find all supernova-host galaxy matches within a certain search radius (in arcseconds)
+coneSearchPairs(supernovaCoord[0], 1.e3)
