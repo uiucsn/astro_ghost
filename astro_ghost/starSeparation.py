d77feca0d74a62d45d8ba389364b342d297e713d
alexandergagliano <alexandertgagliano@gmail.com>
2023-05-21 23:43:50 -0400

    fix typo
    Former-commit-id: 77879962cce01c9b846a7fbde2ea05929c5120c2

--- astro_ghost/starSeparation.py
+++ astro_ghost/starSeparation.py
@@ -156,7 +156,7 @@ def separateStars_South(df, plot=0, verbose=0, starcut='gentle'):
     except:
         print("Error! I didn't understand your starcut option.")
 
-    test_gals = unsure_dropped[~unsure_dropped.index.isin(df_stars.index)]
+    test_gals = unsure_dropped[~unsure_dropped.index.isin(test_stars.index)]
 
     # Adding back in the ones we identified as galaxies from our clustering above, and the NED sources.
     df_gals = pd.concat([test_gals, only_na, NED_gals])
